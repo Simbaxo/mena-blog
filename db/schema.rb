5f1a74e0803cf6e1a0a9cf0ec4df96054661cf13
Andres <ballerlikemike23@gmail.com>
2017-08-13 20:11:05 -0400

    Fix ckeditor

--- db/schema.rb
+++ db/schema.rb
@@ -33,12 +33,15 @@ ActiveRecord::Schema.define(version: 20170813214235) do
     t.string "data_file_name", null: false
     t.string "data_content_type"
     t.integer "data_file_size"
+    t.integer "assetable_id"
+    t.string "assetable_type", limit: 30
     t.string "type", limit: 30
     t.integer "width"
     t.integer "height"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["type"], name: "index_ckeditor_assets_on_type"
+    t.index ["assetable_type", "assetable_id"], name: "idx_ckeditor_assetable"
+    t.index ["assetable_type", "type", "assetable_id"], name: "idx_ckeditor_assetable_type"
   end
 
   create_table "posts", force: :cascade do |t|
